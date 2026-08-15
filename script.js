622227758753ab209c3bd1bae698541010f8df76
sibilaimage-osp <sibilaimage@gmail.com>
2026-08-15 16:58:10 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,33 @@
-document.getElementById("startButton").addEventListener("click", function () {
+document.getElementById("kapitel1").addEventListener("click", function () {
 
-    alert("Willkommen bei Lyra Lernkarten! 🎉");
+    document.getElementById("kartenBereich").innerHTML = `
+
+        <div class="karte">
+
+            <h2>🃏 Kapitel 4 – Karte 1</h2>
+
+            <p class="frage">
+                Was bedeutet GoB?
+            </p>
+
+            <button class="antwort">
+                A) Gesetz über Banken
+            </button>
+
+            <button class="antwort">
+                B) Grundsätze ordnungsgemäßer Buchführung
+            </button>
+
+            <button class="antwort">
+                C) Gewinn ohne Bilanz
+            </button>
+
+            <button class="antwort">
+                D) Grundordnung der Betriebe
+            </button>
+
+        </div>
+
+    `;
 
 });
